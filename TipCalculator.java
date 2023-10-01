3e5bffcaf6a0760465eb21833393465ffb499e7f
Sample <justinw279@nycstudents.net>
2023-10-01 19:51:20 -0400

    Update TipCalculator.java

--- TipCalculator.java
+++ TipCalculator.java
@@ -5,7 +5,7 @@ public class TipCalculator {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayList<String> listOfItems = new ArrayList<String>();
+        ArrayList<String> listOfItems = new ArrayList<String>(); // Two arraylists -- one for the items and other for prices.
         ArrayList<Double> listOfPrices = new ArrayList<Double>();
 
         String item;
@@ -19,7 +19,7 @@ public class TipCalculator {
         double tipPercentage = scan.nextDouble();
         scan.nextLine();
 
-        double totalCost = 0;
+        double totalCost = 0; // Declare and init variable.
         double totalBillBeforeTip = 0;
         double finalBill = 0;
         double totalTip = 0;
@@ -27,7 +27,7 @@ public class TipCalculator {
         double tipPerPerson = 0;
         double totalCostPerPerson = 0;
 
-        while (price != -1) {
+        while (price != -1) { // Gets input for each menu item unless -1, otherwise breaks
             System.out.print("Enter a cost in dollars and cents, -1 to end: ");
             price = scan.nextDouble();
             if (price == -1) {
@@ -40,28 +40,35 @@ public class TipCalculator {
             listOfItems.add(item);
         }
 
-        for(int i = 0; i < listOfPrices.size(); i++) {
+        for(int i = 0; i < listOfPrices.size(); i++) { // Iterates thru list and adds prices
             totalBillBeforeTip += listOfPrices.get(i);
-        }
-        totalBillBeforeTip = (double) Math.round(totalBillBeforeTip * 100) / 100;
+        } 
+
+        // All variables calculated here
+        totalBillBeforeTip = (double) Math.round(totalBillBeforeTip * 100) / 100; // Rounding to the nearest cent
+        totalTip = totalBillBeforeTip * (tipPercentage / 100);
+        totalTip =  (double) Math.round(totalTip * 100) / 100; // Rounding to the nearest cent
+        totalCost = totalTip + totalBillBeforeTip;
+        totalCost =  (double) Math.round(totalCost * 100) / 100; // Rounding to the nearest cent
+        perPersonCostBeforeTip = totalBillBeforeTip / peopleInGroup;
+        perPersonCostBeforeTip = Math.round(perPersonCostBeforeTip * 100) / 100.0; // Rounding to the nearest cent       
+        tipPerPerson = totalTip / peopleInGroup;
+        tipPerPerson =  (double) Math.round(tipPerPerson * 100) / 100; // Rounding to the nearest cent         
+        totalCostPerPerson = totalCost / peopleInGroup;
+        totalCostPerPerson =  (double) Math.round(totalCostPerPerson * 100) / 100; // Rounding to the nearest cent
+        
+        // Prints all variables out
         System.out.println("------------------------------\nTotal bill before tip: $" + totalBillBeforeTip);
         System.out.println("Tip percentage: $" + tipPercentage);
-        totalTip = totalBillBeforeTip * (tipPercentage / 100);
-        totalTip =  (double) Math.round(totalTip * 100) / 100;
         System.out.println("Total tip: $" + totalTip);
-        totalCost = totalTip + totalBillBeforeTip;
-        totalCost =  (double) Math.round(totalCost * 100) / 100;
         System.out.println("Total bill with tip: $" + totalCost);
-        perPersonCostBeforeTip = totalBillBeforeTip / peopleInGroup;
-        perPersonCostBeforeTip = Math.round(perPersonCostBeforeTip * 100) / 100.0;
         System.out.println("Per person cost before tip: $" + perPersonCostBeforeTip);
-        tipPerPerson = totalTip / peopleInGroup;
-        tipPerPerson =  (double) Math.round(tipPerPerson * 100) / 100;
         System.out.println("Tip per person: $" + tipPerPerson);
-        totalCostPerPerson = totalCost / peopleInGroup;
-        totalCostPerPerson =  (double) Math.round(totalCostPerPerson * 100) / 100;
         System.out.println("Total cost per person: $" + totalCostPerPerson + "\n------------------------------");
         System.out.println("Items ordered: ");
+
+
+        // Prints each item and corresponds the same index to the list of items.
         for(int i = 0; i < listOfItems.size(); i++) {
             System.out.println(listOfItems.get(i) + ", costed $" + listOfPrices.get(i));
         }
